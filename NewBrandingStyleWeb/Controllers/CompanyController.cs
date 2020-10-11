e5f3b94839f47b024ebc3cfd8bee9111205d0027
Przemysław Sakowski <p.sakowski@protonmail.com>
2020-10-11 17:51:18 +0200

    add form submiting

---
+++ NewBrandingStyleWeb/Controllers/CompanyController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using NewBrandingStyleWeb.Models;
+
+namespace NewBrandingStyleWeb.Controllers
+{
+    public class CompanyController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(CompanyModel company)
+        {
+            var viewModel = new CompanyAddedViewModel
+            {
+                NumberOfCharsInName = company.Name.Length,
+                NumberOfCharsInDescription = company.Description.Length,
+                IsHidden = !company.IsVisible
+            };
+
+            return View("CompanyAdded", viewModel);
+        }
+    }
+}
