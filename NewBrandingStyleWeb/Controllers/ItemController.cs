df69db400532dca6484534c4839faca3ec9ce6fc
Przemysław Sakowski <p.sakowski@protonmail.com>
2020-11-11 12:21:10 +0100

    add api controller & fetch form version

---
+++ NewBrandingStyleWeb/Controllers/ItemController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using NewBrandingStyleWeb.Models;
+
+namespace NewBrandingStyleWeb.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ItemController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Post([FromForm] CompanyModel company)
+        {
+            return Ok();
+        }
+    }
+}
---
+++ NewBrandingStyleWeb/wwwroot/js/addNewItem.js
@@ -0,0 +1,29 @@
+﻿(function () {
+    const alertElement = document.getElementById("success-alert");
+    const formElement = document.forms[0];
+
+    const addNewItem = async () => {
+        // 1. read data from the form
+        const requestData = new FormData(formElement);
+        // 2. call the application server using fetch method
+        const response = await fetch("api/Item",
+            {
+                body: requestData,
+                method: "post"
+            });
+        console.log(response);
+
+        const responseJson = await response.json();
+
+        if (responseJson.success) {
+            alertElement.style.display = 'block';
+        }
+    };
+
+    window.addEventListener("load", () => {
+        formElement.addEventListener("submit", event => {
+            event.preventDefault();
+            addNewItem().then(() => console.log("added successfully"));
+        });
+    });
+})();
